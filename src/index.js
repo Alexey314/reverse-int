79696d72776cf8983f98ad85d0e7ad0e984a4599
apotapov <apotapovxx@mail.ru>
2021-02-02 21:03:22 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0)
+        return +("" + n).split("").reverse().join("");
+    else
+        return +("" + -n).split("").reverse().join("");
 }
